549ad76d4dcb56c3895aa0b297ee2cc38c2f66b4
InitusNovus <initusnovus@gmail.com>
2023-05-01 03:41:38 +0900

    Problem 7 solved

---
+++ Problem7/Program.fsx
@@ -0,0 +1,15 @@
+﻿//Implementation
+let reverseList lst =
+    let rec innerLoop lst res = 
+        match lst with 
+        | [] -> res
+        | head::tail -> 
+            head::res |> innerLoop tail
+    innerLoop lst []
+
+//Test
+let testList =  [1; 2; 3; 4; 5]
+testList |> reverseList |> (printfn "%A")
+
+//In practice, use "List.rev."
+testList |> List.rev |> (printfn "%A")
